03354a796905753372e5ec36727366992adcb9c7
jqnfxa <ivan.boriso2014@gmail.com>
2023-10-22 09:52:33 +0300

    cmaked-build-debug deleted

--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shard/CLionProjects/Algorithms_lb3/lib/googletest/googletest
-# Build directory: /home/shard/CLionProjects/Algorithms_lb3/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
